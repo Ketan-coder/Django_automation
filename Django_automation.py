6fe42027d69a30e5a7a6281c38d70308198b59e5
Ketan <ketanv288@gmail.com>
2022-02-08 20:16:05 +0530

    Creating templates and static folders, other minor changes

--- Django_automation.py
+++ Django_automation.py
@@ -1,5 +1,7 @@
+""" This python file helps me to make full flushed static and templates directories """
 import os
 
+print("Please do not use space in your project/app name at the end!")
 project_name = str(input("Whats your project name : ")).capitalize()
 apps_name = str(input("What your apps name : ")).split(",")
 
@@ -39,6 +41,57 @@ print(replace_text)
 # append the apps name to INSTALLED_APPS in settings.py
 with open(project_name + '/settings.py', 'r+') as file:
     content=file.read()
-    new_file = '\n\n#----------------Replace INSTALLED_APPS with the below code----------------\/\n' + replace_text
+    new_file = '\n\n#----------------Replace INSTALLED_APPS with the below code----------------\/\n' + replace_text + "\nimport os\nSTATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]\n"
     file.write(new_file)
     file.truncate()
+
+os.chdir('../')
+# print the current directory
+print(os.getcwd())
+
+for app_name in apps_name:
+    with open(project_name + '/' + app_name.capitalize() + '/views.py', 'r+') as file:
+        content=file.read()
+        new_file = 'def index(request):\n    return render("index.html")\n'
+        file.write(new_file)
+        file.truncate()
+    with open(project_name + '/' + app_name.capitalize() + '/urls.py', 'w+') as file:
+        content=file.read()
+        new_file = 'from django.urls import path\nfrom . import views\n\nurlpatterns = [\n    path("", views.index, name="index"),\n]\n'
+        file.write(new_file)
+        file.truncate()
+    with open(project_name + '/' + project_name + '/urls.py', 'r+') as file:
+        content=file.read()
+        for app_name in apps_name:
+            content = content.replace(']','include("",(' + app_name.capitalize() + '.urls")),\n]')
+        # new_file = "from "+ app_name.capitalize() +" import views\n\nurlpatterns = [\n    path('admin/', admin.site.urls),\n    path('',include('"+ app_name.capitalize() +".urls')\n]\n"
+        file.write(content)
+        file.truncate()
+
+os.chdir(project_name)
+# print the current directory
+print(os.getcwd())
+# create the templates,static directory
+os.makedirs("templates")
+os.makedirs("static")
+
+html_files=['index.html','base.html','about.html','contact.html','login.html','newuser.html']
+
+dirs =['resoures', 'vendors']
+sub_dirs = ["css","img","js"]
+
+for html_file in html_files:
+    open("templates/"+html_file, "w")
+
+dirs =['resoures', 'vendors']
+
+for dires in dirs:
+    os.makedirs("static/" + dires)
+
+for sub_dir in sub_dirs:
+    os.makedirs("static/resoures/" + sub_dir)
+
+os.makedirs("static/resoures/css/" + sub_dirs[1])
+
+for sub_dir in sub_dirs:
+    os.makedirs("static/vendors/" + sub_dir)
